24c084cd40f2735acec3a8b6a2201d778ca2dd79
Max Angelma <max@angelma-makela.fi>
2017-04-19 17:18:18 +0300

    fixed paper loading

--- paperStation.js
+++ paperStation.js
@@ -22,32 +22,32 @@ function subscribeToEvents() {
 
   console.log("Subscribed to PaperLoaded!");
   request.post('http://localhost:3000/RTU/SimROB1/events/PaperLoaded/notifs',
-    {form:{destUrl:"http://localhost:" + port}}, function(err,httpResponse,body){
+    {form:{destUrl:"http://localhost:" + port + "/events"}}, function(err,httpResponse,body){
     });
   console.log("Subscribed to PaperUnloaded!");
   request.post('http://localhost:3000/RTU/SimROB1/events/PaperUnloaded/notifs',
-    {form:{destUrl:"http://localhost:" + port}}, function(err,httpResponse,body){
+    {form:{destUrl:"http://localhost:" + port + "/events"}}, function(err,httpResponse,body){
       //console.log(err);
       //console.log(body);
       //console.log(httpResponse);
     });
   console.log("Subscribed to Z1_Changed!");
   request.post('	http://localhost:3000/RTU/SimCNV1/events/Z1_Changed/notifs',
-    {form:{destUrl:"http://localhost:" + port}}, function(err,httpResponse,body){
+    {form:{destUrl:"http://localhost:" + port + "/events"}}, function(err,httpResponse,body){
       //console.log(err);
       //console.log(body);
       //console.log(httpResponse);
     });
   console.log("Subscribed to Z2_Changed!");
   request.post('	http://localhost:3000/RTU/SimCNV1/events/Z2_Changed/notifs',
-    {form:{destUrl:"http://localhost:" + port}}, function(err,httpResponse,body){
+    {form:{destUrl:"http://localhost:" + port + "/events"}}, function(err,httpResponse,body){
       //console.log(err);
       //console.log(body);
       //console.log(httpResponse);
     });
   console.log("Subscribed to Z3_Changed!");
   request.post('	http://localhost:3000/RTU/SimCNV1/events/Z3_Changed/notifs',
-    {form:{destUrl:"http://localhost:" + port}}, function(err,httpResponse,body){
+    {form:{destUrl:"http://localhost:" + port + "/events"}}, function(err,httpResponse,body){
       //console.log(err);
       //console.log(body);
       //console.log(httpResponse);
@@ -115,8 +115,8 @@ function UpdatePalletInformation (recipe, paperBool, destination, ID) {
         "fcolor" : uprecept[3],
         "scolor" : uprecept[4],
         "kcolor" : uprecept[5],
-        "destination": destination,
         "hasPaper" : paperBool,
+        "destination": destination
       }
     }
   };
@@ -128,13 +128,13 @@ function UpdatePalletInformation (recipe, paperBool, destination, ID) {
   });
 };
 
-// POST message handling
+// POSTs coming from WS7
 app.post('/', function(req, res) {
     //console.log(req.body.id + " received!");
 
     // Parse incoming POST message body
     var data = JSON.stringify(req.body)
-    //console.log(data);
+    console.log("root received: " + data);
     data = replaceall("{", "", data)
     data = replaceall("}", "", data)
     data = replaceall('"', "", data)
@@ -144,34 +144,50 @@ app.post('/', function(req, res) {
     data = replaceall("&", ",", data)
     var datatable = data.split(",")
 
+    console.log("frame:");
+    console.log(req.body.frame);
+
     // Gather recipe values used in UpdatePalletInformation
     var recipe = datatable[1]+","+datatable[3]+","+datatable[5]+","+datatable[7]+","+datatable[9]+","+datatable[11];
 
-    // Let's move the pallet straight to Z3 where pallet details will be checked
-    if ((req.body.id == 'Z1_Changed') && (req.body.payload.PalletID != -1)) {
-      TransZone("12");
-    }
-    else if ((req.body.id == 'Z2_Changed') && (req.body.payload.PalletID != -1)) {
-      TransZone("23");
-    }
-    else if ((req.body.id == 'Z3_Changed') && (req.body.payload.PalletID != -1) &&
-        (datatable[1] != '0') && (datatable[3] != '0') && (datatable[5] != '0')) {
-      GetPalletInformation(req.body.payload.PalletID); // Pallet status?
-      console.log("Pallet without paper, loading a piece...");
-      LoadPaper();
-      UpdatePalletInformation(recipe,'1','2',datatable[17]); // Update pallet information, destination always 2
-    }
-    // Check if product on pallet is finished
-    else if((datatable[1] == '0') && (datatable[3] == '0') && (datatable[5] == '0')) {
+    if (req.body.frame == undefined) {
+        //console.log("empty body");
+        return;
+    } else if ( (req.body.frame != '0') && (req.body.screen != '0') && (req.body.keyboard != '0') ) {
+        console.log("Pallet without paper, loading a piece...");
+        LoadPaper();
+        UpdatePalletInformation(recipe,'1','2',datatable[17]); // Update pallet information, destination always 2
+    } else if ( (req.body.frame == '0') && (req.body.screen == '0') && (req.body.keyboard == '0') ) {
         console.log('Product ready, removing...');
         UnloadPaper();
         UpdatePalletInformation(recipe, '0', '2', datatable[17]);
+    } else {
+        TransZone("35");
+    }
+
+    res.end('\n end of response');
+  });
+
+// Event handling from simulator
+app.post('/events', function(req, res) {
+
+    //console.log("/events received: " + JSON.stringify(req.body));
+
+    // Let's move the pallet straight to Z3 where pallet details will be checked
+    if ((req.body.id == 'Z1_Changed') && (req.body.payload.PalletID != -1)) {
+        TransZone("12");
+    }
+    else if ((req.body.id == 'Z2_Changed') && (req.body.payload.PalletID != -1)) {
+        TransZone("23");
+    }
+    else if ((req.body.id == 'Z3_Changed') && (req.body.payload.PalletID != -1) ) {
+        GetPalletInformation(req.body.payload.PalletID); // Pallet status?
     }
     // Transfer pallet from WS1 to WS2 after loading/unloading
     else if((req.body.id == 'PaperLoaded') || (req.body.id == 'PaperUnloaded')){
-      TransZone('35');
+        TransZone('35');
     }
     res.end('\n Information Exhange Sequence End');
-  });
+});
 
 subscribeToEvents();
