ac774a1026c7fbb8919c01dfb1148dbfd30afb35
Max Angelma <max@angelma-makela.fi>
2017-04-24 23:04:55 +0300

    starting to implement multiple product order handling

--- orderGateway.js
+++ orderGateway.js
@@ -1,4 +1,7 @@
 // Created by Max Angelma on 5.4.2017
+
+// Modified for FIS course on 23.4.2017
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -23,6 +26,35 @@ function send(order) {
         //console.log(body);
         if (error) { console.log(error); }
     });
+}
+
+// JSON-order is sent here
+function schedule(order) {
+
+    console.log("scheduling execution");
+    console.log("Order lenght: " + order.length);
+    //console.log(order[0]);
+    testPrint(order);
+
+    for(var i=0;i<order.length;i++) {
+        setTimeout(testPrint, i*2000);
+    }
+
+    /*
+    request.post({
+        headers: { "content-type" : "application/json" },
+        url: 'http://localhost:4107/order',
+        json: order
+    }, function(error, response, body){
+        //console.log(body);
+        if (error) { console.log(error); }
+    });
+
+    */
+}
+
+function testPrint() {
+    console.log("fuck");
 }
 
 function pollSimulator() {
@@ -62,13 +94,16 @@ io.on('connection', function(socket){
 
     socket.on('moveToProd', function(order){
 
-        console.log("MovetoProd socket io received");
-        console.log(order);
+        //console.log("MovetoProd socket io received");
+        //console.log(order);
 
         if (order != "0") {
             i++;
-            send(order);
-            console.log("Order: " + i);
+            //send(order);
+
+            schedule(order);
+
+            //console.log("Order: " + i);
             //io.emit('emitOrder', order);
 
         }
@@ -120,37 +155,3 @@ app.post('/', function(req, res){
     //console.log(tilaus);
     res.end('\n ordering system received information');
 });
-
-
-// for debugging, not actually needed
-function subscribeToEvents() {
-
-    request.post('http://localhost:3000/RTU/SimCNV8/events/Z1_Changed/notifs',
-        {form:{destUrl:"http://localhost:" + port}}, function(err,httpResponse,body){
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("Subscribed to CNV8 Zone1");
-            }
-        });
-
-    request.post('http://localhost:3000/RTU/SimCNV8/events/Z2_Changed/notifs',
-        {form:{destUrl:"http://localhost:" + port}}, function(err,httpResponse,body){
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("Subscribed to CNV8 Zone2!");
-            }
-        });
-
-    request.post('http://localhost:3000/RTU/SimCNV8/events/Z3_Changed/notifs',
-        {form:{destUrl:"http://localhost:" + port}}, function(err,httpResponse,body){
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("Subscribed to CNV8 Zone3");
-            }
-        });
-}
-
-//subscribeToEvents();
